380d5170256334804d61ec3d4065886059d520e3
Dokaebi20 <96099551+Dokaebi20@users.noreply.github.com>
2021-12-17 13:02:47 +0700

    17/12

--- app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/checkout_done.java
+++ app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/checkout_done.java
@@ -2,7 +2,9 @@ package com.example.a43ug_a11202012434_michaelindrawan_utsppb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class checkout_done extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class checkout_done extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_done);
+    }
+    public void toMainMenu(View view){
+        Intent i = new Intent(checkout_done.this, main_menu.class);
+        startActivity(i);
     }
 }
