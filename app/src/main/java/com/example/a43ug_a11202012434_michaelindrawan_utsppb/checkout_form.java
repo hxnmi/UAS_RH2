19de8e464c3487f6a7ee6ea5351e435c14b5b747
Dokaebi20 <96099551+Dokaebi20@users.noreply.github.com>
2021-12-17 10:48:31 +0700

    17/12

---
+++ app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/checkout_form.java
@@ -0,0 +1,14 @@
+package com.example.a43ug_a11202012434_michaelindrawan_utsppb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkout_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_form);
+    }
+}
