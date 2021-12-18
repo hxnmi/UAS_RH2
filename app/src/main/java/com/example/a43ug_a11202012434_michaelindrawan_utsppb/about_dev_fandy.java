b34782528a6ee145e3ffd8879cc9d4a77566420e
Prakasa2021 <uzumakidef@gmail.com>
2021-12-18 17:26:45 +0700

    about developer

---
+++ app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/about_dev_fandy.java
@@ -0,0 +1,14 @@
+package com.example.a43ug_a11202012434_michaelindrawan_utsppb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class about_dev_fandy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_dev_fandy);
+    }
+}
