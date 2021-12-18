16b7462a4f80b0830a1cd6015c7274e6e81702df
FandN <111202012421@mhs.dinus.ac.id>
2021-12-18 13:41:54 +0700

    layout info

---
+++ app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/Info.java
@@ -0,0 +1,14 @@
+package com.example.a43ug_a11202012434_michaelindrawan_utsppb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
