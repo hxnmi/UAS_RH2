66096ca5c23ee43e93611c5919f8dbbcabe8113e
hxnmi <haniffahmi60@gmail.com>
2022-01-01 10:53:14 +0700

    masih error. updatePassword,validasi & some fixes

--- app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/forgot_password.java
+++ app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/forgot_password.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.transition.TransitionManager;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.Gravity;
 import android.view.View;
@@ -21,8 +22,12 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class forgot_password extends AppCompatActivity {
@@ -34,6 +39,9 @@ public class forgot_password extends AppCompatActivity {
     DatabaseReference reff;
     Users xuser;
     FirebaseAuth fAuth;
+    FirebaseAuth.AuthStateListener fStateListener;
+
+    private static final String TAG = sign_up.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,19 +53,49 @@ public class forgot_password extends AppCompatActivity {
         Container = findViewById(R.id.Container);
         fAuth = FirebaseAuth.getInstance();
 
+        fStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                }
+                else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out:");
+                }
+            }
+        };
 
         BtnSend = findViewById(R.id.SendButton);
         xuser = new Users();
-        reff = FirebaseDatabase.getInstance().getReference().child("User");
 
-        BtnSend.setOnClickListener(new View.OnClickListener(){
+        final DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("User");
+        reff.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View view){
-                sendButton(EditEmailTxt.getText().toString());
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                    String xemail = userSnapshot.child("email").getValue(String.class);
+
+                    BtnSend.setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View view){
+                            sendButton(xemail,EditEmailTxt.getText().toString());
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
+
     }
-    public void sendButton(final String email){
+    public void sendButton(String xemail,final String email){
+
+
+
         if(TextUtils.isEmpty(EditEmailTxt.getText().toString().trim())){
             TransitionManager.beginDelayedTransition(Container);
             EmailEmpty.setVisibility(View.VISIBLE);
@@ -69,31 +107,14 @@ public class forgot_password extends AppCompatActivity {
                 EmailEmpty.setVisibility(View.GONE);
                 EmailWrong.setVisibility(View.VISIBLE);
             }
-            else {
+            else if(xemail.equals(email)){
                 EmailEmpty.setVisibility(View.GONE);
                 EmailWrong.setVisibility(View.GONE);
-                
-                fAuth.sendPasswordResetEmail(email)
-                        .addOnSuccessListener(this, new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Toast.makeText(forgot_password.this, "Reset Code Has Been Sent To Your Email",Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(forgot_password.this, reset_password.class);
-                                startActivity(i);
-                            }
-                        })
-                        .addOnFailureListener(this, new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast toast = Toast.makeText(forgot_password.this, "Error! Can't Send The Reset Code.\n" + e.getMessage(),Toast.LENGTH_LONG);
-                                LinearLayout layout = (LinearLayout) toast.getView();
-                                if (layout.getChildCount() > 0) {
-                                    TextView tv = (TextView) layout.getChildAt(0);
-                                    tv.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                                }
-                                toast.show();
-                            }
-                        });
+
+                Intent i = new Intent(forgot_password.this, reset_password.class);
+                startActivity(i);
+
+
             }
     }
     private static boolean isEmailValid(CharSequence email){
--- app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/reset_password.java
+++ app/src/main/java/com/example/a43ug_a11202012434_michaelindrawan_utsppb/reset_password.java
@@ -1,39 +1,108 @@
 package com.example.a43ug_a11202012434_michaelindrawan_utsppb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class reset_password extends AppCompatActivity {
     EditText Edit6DigitsCode;
+    EditText EditPassword;
     EditText EditNewPassword;
     EditText ConfirmNewPassword;
     TextView PasswordandCodeEmpty;
     TextView PasswordnotMatch;
     ViewGroup Container;
     TextView CodenotMatch;
+    Button BtnConfirm;
+    DatabaseReference reff;
+    FirebaseUser user;
+    FirebaseAuth fAuth;
+    FirebaseAuth.AuthStateListener fStateListener;
+
+
+    private static final String TAG = reset_password.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
         Edit6DigitsCode = findViewById(R.id.Edit6DigitsCode);
+        EditPassword = findViewById(R.id.EditPassword);
         EditNewPassword = findViewById(R.id.EditNewPassword);
         ConfirmNewPassword = findViewById(R.id.ConfirmNewPassword);
         PasswordandCodeEmpty= findViewById(R.id.PasswordandCodeEmpty);
         PasswordnotMatch = findViewById(R.id.PasswordnotMatch);
         CodenotMatch = findViewById(R.id.CodenotMatch);
         Container = findViewById(R.id.Container);
+        fAuth = FirebaseAuth.getInstance();
+
+        fStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                }
+                else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out:");
+                }
+            }
+        };
+
+
+        user = fAuth.getCurrentUser();
+        reff = FirebaseDatabase.getInstance().getReference().child("User");
+
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                String oldPassword = dataSnapshot.child("password").getValue().toString();
+
+                BtnConfirm.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        resetPassword(oldPassword, EditPassword.getText().toString(),EditNewPassword.getText().toString());
+                    }
+
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+
     }
-    public void resetPassword(View view){
+
+    public void resetPassword(String oldPassword,String edtPassword, final String newPassword){
+
+
         if((TextUtils.isEmpty(Edit6DigitsCode.getText().toString().trim()))||(TextUtils.isEmpty(EditNewPassword.getText().toString().trim()))||TextUtils.isEmpty(ConfirmNewPassword.getText().toString().trim())){
             TransitionManager.beginDelayedTransition(Container);
             PasswordandCodeEmpty.setVisibility(View.VISIBLE);
@@ -54,14 +123,41 @@ public class reset_password extends AppCompatActivity {
                 PasswordnotMatch.setVisibility(View.GONE);
                 CodenotMatch.setVisibility(View.VISIBLE);
             }
-            else{
+            else if(edtPassword.equals(oldPassword)){
                 PasswordandCodeEmpty.setVisibility(View.GONE);
                 PasswordnotMatch.setVisibility(View.GONE);
                 CodenotMatch.setVisibility(View.GONE);
-                Intent i = new Intent(reset_password.this, successful_reseting_password.class);
-                startActivity(i);
-            }
 
+
+                    user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Password updated");
+                                Intent i = new Intent(reset_password.this, successful_reseting_password.class);
+                                startActivity(i);
+                            } else {
+                                Log.d(TAG, "Error password not updated");
+                            }
+                        }
+                    });
+
+                 }
+            }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fAuth.addAuthStateListener(fStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (fStateListener != null) {
+            fAuth.removeAuthStateListener(fStateListener);
         }
     }
+
 }
